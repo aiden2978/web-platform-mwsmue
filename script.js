97a437b28e3695d8521d6dfa8c07960f5191c70f
aiden2978 <71123921+aiden2978@users.noreply.github.com>
2022-10-09 15:25:31 +0900

    3 files modified

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById("modal_open").onclick = function(){
+  document.getElementById("modal").style.display="block";
+}
+
+document.getElementById("modal_close").onclick = function(){
+  document.getElementById("modal").style.display="none";
+}
