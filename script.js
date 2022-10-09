8b07c83075ffe2738e49262608ab1dfd4d6e55b6
aiden2978 <71123921+aiden2978@users.noreply.github.com>
2022-10-09 15:49:17 +0900

    6 files modified

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 document.getElementById("modal_open").onclick = function(){
-  document.getElementById("modal").style.display="block";
+  document.getElementById("modal").style.display="flex";
 }
 
 document.getElementById("modal_close").onclick = function(){
